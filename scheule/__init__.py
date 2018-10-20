d069ff270a66613a93be6bca7f9efba9350e7437
tentenlee <tentenlee100@gmail.com>
2018-10-20 21:51:00 +0800

    [Update] 新增開始時間與結束時間

--- scheule/__init__.py
+++ scheule/__init__.py
@@ -3,4 +3,5 @@ from scheule.Team8 import *
 from scheule.Ske import *
 from scheule.Nmb import *
 from scheule.Hkt import *
+from scheule.Ngt import *
 from scheule.Stu import *
--- scheule/dataType/Schedule.py
+++ scheule/dataType/Schedule.py
@@ -2,4 +2,6 @@
 class Schedule(object):
     type = ""  # 類別  活動 or 電視 等等 可用日文
     title = ""  # 行事曆標題
+    start_time = ""  # 開始時間HH:mm (日本時間)  24小時製  EX: 10:00
+    end_time = ""  # 結束時間 HH:mm (日本時間)   24小時製  若沒資料回寫入空白字串 EX: 10:00
     members = []  # 參加活動的成員，若沒有可不塞值
