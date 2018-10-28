73f20c624e69bcba9db8716200153df5744e1fbc
grtfou <1241272+grtfou@users.noreply.github.com>
2018-10-28 19:36:49 +0800

    Edit: typo and basic test function

--- schedule/dataType/Schedule.py
+++ schedule/dataType/Schedule.py
@@ -2,7 +2,7 @@ import json
 
 
 class Schedule(object):
-    type = ""  # 類別  活動 or 電視 等等 可用日文
+    event_type = ""  # 類別  活動 or 電視 等等 可用日文
     title = ""  # 行事曆標題
     start_time = ""  # 開始時間HH:mm (日本時間)  24小時製  EX: 10:00
     end_time = ""  # 結束時間 HH:mm (日本時間)   24小時製  若沒資料回寫入空白字串 EX: 10:00
@@ -10,10 +10,10 @@ class Schedule(object):
 
     def __repr__(self):
         return "<Schedule type:%s title:%s start_time:%s end_time:%s members:%s>" % (
-        self.type, self.title, self.start_time, self.end_time, str(self.members))
+        self.event_type, self.title, self.start_time, self.end_time, str(self.members))
 
     def __str__(self):
-        dic = dict(type=self.type,
+        dic = dict(type=self.event_type,
                    title=self.title,
                    start_time=self.start_time,
                    end_time=self.end_time,
